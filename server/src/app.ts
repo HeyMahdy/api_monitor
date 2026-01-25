0036826fdef06ff592930ab4cf7f2ba262ba6f73
Mahdy <heymahdymuzzammil@gmail.com>
2026-01-26 00:47:33 +0600

    add another cros

--- server/src/app.ts
+++ server/src/app.ts
@@ -13,7 +13,7 @@ import swaggerSpec from './swagger.js';
 const app = express();
 
 app.use(cors({
-  origin: ['https://api-monitoring-frontend.vercel.app', 'https://api-monitoring-frontend-kvmrnx0ls-mahdynafi1221-5035s-projects.vercel.app'],
+  origin: ['https://pulse-guard-api.lovable.app', 'https://api-monitoring-frontend-kvmrnx0ls-mahdynafi1221-5035s-projects.vercel.app'],
   credentials: true
 }));
 app.use(express.json());
